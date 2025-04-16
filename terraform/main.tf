17b81770ae7f56afe43da63cb3242a226b4e5d21
yashhsoni <128360447+yashhsoni@users.noreply.github.com>
2025-04-16 15:27:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,107 @@
-provider "aws" {
-  region = var.region
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.2"
-
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${var.region}a", "${var.region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.27"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.medium"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
     }
   }
+  required_version = ">=1.0.0"
+}
+
+provider "azurerm" {
+  subscription_id = "61bd38d2-51fa-4243-9689-7f55df510fc1"
+  tenant_id       = "b0b76d6e-14b7-4c08-8657-a19ef23b3b0b"
+  features {}
+}
+
+# Variables
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "myResourceGroup"
+}
+
+variable "acr_name" {
+  default = "acrrathore01" 
+}
+
+variable "aks_cluster_name" {
+  default = "myAKSCluster"
+}
+
+variable "dns_prefix" {
+  default = "myakscluster"
+}
+
+variable "node_count" {
+  default = 2
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Azure Kubernetes Service (AKS)
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
 
   tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    Environment = "Development"
   }
+}
+
+# Role assignment to allow AKS to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+
+ 
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
